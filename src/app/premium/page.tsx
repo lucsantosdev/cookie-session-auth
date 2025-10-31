aa303b2686e3ac28aac8dde7e3be8631e20d7493
lucsantosdev <lucsantosdev@gmail.com>
2025-10-30 21:04:23 -0300

    protected content to the premium page added

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,7 +1,34 @@
-const PremiumPage = () => {
+import { getSession } from "@/actions";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+const PremiumPage = async () => {
+
+  const session = await getSession();
+
+  if (!session.isLoggedIn) {
+    redirect("/")
+  }
+
+  if (!session.isPremium) {
+    return (
+      <div className="notPremium">
+        <h1>You need to be a premium user to access this page.</h1>
+        <p>Please upgrade your account to access premium features.</p>
+        <Link href="/profile">Go to Profile</Link>
+      </div>
+    );
+  }
+
   return (
     <div className="premium">
       <h1>Welcome to the PremiumPage</h1>
+      <p>You have access to premium features!</p>
+      <ul>
+        <li>Exclusive content #1</li>
+        <li>Exclusive content #2</li>
+        <li>Exclusive content #3</li>
+      </ul>
     </div>
   )
 }
